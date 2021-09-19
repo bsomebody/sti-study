95055c6c9a35ad503e746f25c206f3e7eeaf6bd2
bsomebody <90913769+bsomebody@users.noreply.github.com>
2021-09-19 09:53:58 +0800

    third

--- 源.c
+++ 源.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 bool isMatch(char* s, char* p) {
-    if (*s == '\0' && *p == '\0') return true;//s和p均为空，匹配成功
-    if (*p == '\0') return false;//s非空，p 空，失败
-    //s,p非空时情况
-    int flag = false;
-    if (*s != '\0' && (*s == *p || *p == '.')) flag = true;//s和p均非空， 相等或p为‘.’时匹配成功
-    //当p的下一位为‘*‘时，*代表0个则s中该字母与p中*后字母比较，*代表多个则s中下一个字母与p中该字母比较
-    if (*(p + 1) == '*') {
-        return isMatch(s, p + 2) || flag && isMatch(s + 1, p);
+    int sl = strlen(s);
+    int pl = strlen(p);
+    bool dp[20][30];
+    memset(dp, 0, sizeof(dp));
+    for (int i = sl; i > -1; --i)
+    {
+        for (int j = pl; j > -1; --j)
+        {
+            if (i == sl && j == pl)
+            {
+                dp[i][j] = true;
+                continue;
+            }
+            if (pl - j > 1 && p[j + 1] == '*')// 第一种是带*的情况
+            {
+                dp[i][j] = dp[i][j + 2] || (i < sl && (p[j] == '.' || p[j] == s[i]) && dp[i + 1][j]);// *可能为零个，所以第一种情况是零的情况，直接判断之前的情况通不通过，是不是真，第二种就是有相同的，符号.或者是两者相同，都可以为true
+            }
+            else
+            {
+                dp[i][j] = i < sl && (p[j] == '.' || p[j] == s[i]) && dp[i + 1][j + 1];// 正常情况，符号.或者是两者相同则为真
+            }
+        }
     }
-    //若p下一位不为’*‘，则s，p均向后一位进行比较
-    else {
-        return flag && isMatch(s + 1, p + 1);
-    }
+
+    return dp[0][0];//最后判断到第一位，如果还是有为真的情况，这证明有一条以上的路是同的
 }
+
 
 int main()
 {
     bool isMatch(char* s, char* p);
-    char s[]= "mississippi";
-    char p[]= "mis*is*p*.";
+    char s[] = "mississippi";
+    char p[] = "mis*is*ip*.";
     printf("%d", isMatch(s, p));
 }
